464ae6bc9a05b62f4601808aa416ce2bb23a56a4
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-08-05 21:36:36 -0500

    controller.js updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,38 +16,42 @@ sequelizeConnection.sync({ force:true });
 // Routes
 module.exports = function(app) {
 	// GET route
-	app.get("/api/burgers", function(req, res) {
-		// findAll returns all entries for a table when used with no options
+	app.get("/", function(req, res) {
+		// findAll returns all entries for a table (bugers) when used with no options
 		db.Burger.findAll({}).then(function(dbBurger) {
+
+			var burgerObj = {
+				burgers: dbBurger
+			};
 			// We have access to the burgers as an argument inside of the callback function.
-			res.json(dbBurger);
+			res.render("index", burgerObj);
 		});
 	});
 
 	// POST route to making/saving a new Burger
-	app.post("/api/burgers", function(req, res) {
+	app.post("/", function(req, res) {
 		// Create takes an argument of an object describing the item/burger we want to insert into our table. In this case we want to pass an object with the burger name and devoured property.
 		db.Burger.create({
 			burger_name: req.body.burger_name,
 			devoured: req.body.devoured
 		}).then(function(dbBurger){
 			// We have access to the burgers as an argument inside of the callback function.
-			res.json(dbBurger);
+			res.redirect("/";
 		});
 	});
 
 	// PUT route for updating a burger's devoured status
-	app.put("/api/burgers", function(req, res) {
+	app.put("/:id", function(req, res) {
 		// Update takes in 2 arguments, an object describing the properties we want to update, and a "where" object describing the Burger we want to update
 		db.Burger.update({
 			devoured: req.body.devoured
 		}, {
 			where: {
-				id: req.body.id
+				id: req.params.id
 			}
 		}).then(function(dbBurger) {
 			// We have access to the burgers as an argument inside of the callback function.
-			res.json(dbBurger);
+			res.redirect("/";
 		});
 	});
 };
